333f439080a895cec0fc781e21a2c193df2e2a1e
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-10-16 11:52:20 +0530

    Stuck at getting numbers out of the value

--- Assignments/DOM/II/Eval Report/index.js
+++ Assignments/DOM/II/Eval Report/index.js
@@ -9,14 +9,16 @@ function getInfo(){
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     td1.innerText=name;
-    let td2 = document.createElement("tr");
+    let td2 = document.createElement("td");
     td2.innerText=batch;
-    let td3 = document.createElement("tr");
+    let td3 = document.createElement("td");
     td3.innerText=dsa;
-    let td4 = document.createElement("tr");
+    let td4 = document.createElement("td");
     td4.innerText=skill;
-    let td5 = document.createElement("tr");
+    let td5 = document.createElement("td");
     td5.innerText=coding;
-
+    tr.append(td1,td2,td3,td4,td5)
+    document.querySelector("tbody").append(tr)
+    
 }
 
