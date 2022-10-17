a187fe666e5961c89d6a93e9079835b4c71928e2
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-10-17 21:42:09 +0530

    Completed the evaluations amap

--- Evaluation/Unit 4 Coding Evaluation (17 Oct 2022)/Scripts/booking.js
+++ Evaluation/Unit 4 Coding Evaluation (17 Oct 2022)/Scripts/booking.js
@@ -1,1 +1,6 @@
 // Write All `All Movies`  Page Script Here
+arr = JSON.parse(localStorage.getItem(arr));
+console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
--- Evaluation/Unit 4 Coding Evaluation (17 Oct 2022)/Scripts/index.js
+++ Evaluation/Unit 4 Coding Evaluation (17 Oct 2022)/Scripts/index.js
@@ -1,1 +1,24 @@
 // Write All Home  Page Script Here
+var arr=[]
+
+document.querySelector('input[type="submit"]').addEventListener("click",setData);
+function setData(){
+    event.preventDefault();
+
+    let obj = {};
+    let name = document.getElementById("name").value;
+    obj["name"]=name;
+    let mainActor = document.getElementById("mainActor").value;
+    obj["mainActor"]=mainActor;
+    let desc = document.getElementById("desc").value;
+    obj["desc"]=desc;
+    let date = document.getElementById("release").value;
+    obj["date"]=date;
+    let category = document.getElementById("category").value;
+    obj["category"]=category;
+    let price = document.getElementById("price").value;
+    obj["price"]=price;
+    arr.push(obj);
+    localStorage.setItem("arr",JSON.stringify(arr));
+    console.log(arr);
+}
