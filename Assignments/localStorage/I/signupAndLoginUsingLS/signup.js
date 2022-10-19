efe33a17863f735b24200fe35807ef34ef0f0195
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-10-19 14:09:46 +0530

    Complete the redirect function

--- Assignments/localStorage/I/signupAndLoginUsingLS/signup.js
+++ Assignments/localStorage/I/signupAndLoginUsingLS/signup.js
@@ -1,11 +1,13 @@
 console.log("ok")
 document.querySelector('input[type="submit"]').addEventListener("click",setData)
 function setData(){
-    event.preventDefault();
+    event.preventDefault()
     let name = document.querySelector('input[type="text"]').value;
     let mail = document.querySelector('input[type="email"]').value;
     let password = document.querySelector('input[type="password"]').value;
     localStorage.setItem("name",name);
     localStorage.setItem("mail",mail);
     localStorage.setItem("password",password);
+    alert("Your account has been created")
+    window.location.href = "login.html";
 }
