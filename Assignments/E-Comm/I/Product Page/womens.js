d25acadec375d75ce82a60cceea6f8ae523dda95
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-10-20 23:09:18 +0530

    Completed the womens page

---
+++ Assignments/E-Comm/I/Product Page/womens.js
@@ -0,0 +1,477 @@
+var womensData = [
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PRINT LEGGINGS ",
+      price: 1549,
+      strikedoffprice: 1799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK POLYTIGHT",
+      price: 1249,
+      strikedoffprice: 1749,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK SLEEVE TEE",
+      price: 699,
+      strikedoffprice: 1245,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC LEGGINGS",
+      price: 1399,
+      strikedoffprice: 2799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC TIGHTS",
+      price: 1099,
+      strikedoffprice: 1249,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK SHOW SHOES",
+      price: 1249,
+      strikedoffprice: 1489,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S CLASSIC REEBOK PANTS",
+      price: 1459,
+      strikedoffprice: 1235,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING RUN ESSENTIALS SHORT SLEEVE GRAPHIC",
+      price: 849,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK TRAINING WOR MESH TIGHTS",
+      price: 1550,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING OSR HIGH-IMPACT BRA",
+      price: 1399,
+      strikedoffprice: 6599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK TRAINING ACTIVE CORE BACKPACK - MEDIUM",
+      price: 949,
+      strikedoffprice: 1899,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PRINT LEGGINGS ",
+      price: 1549,
+      strikedoffprice: 1799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK POLYTIGHT",
+      price: 1249,
+      strikedoffprice: 1749,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK SLEEVE TEE",
+      price: 699,
+      strikedoffprice: 1245,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC LEGGINGS",
+      price: 1399,
+      strikedoffprice: 2799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC TIGHTS",
+      price: 1099,
+      strikedoffprice: 1249,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK SHOW SHOES",
+      price: 1249,
+      strikedoffprice: 1489,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S CLASSIC REEBOK PANTS",
+      price: 1459,
+      strikedoffprice: 1235,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING RUN ESSENTIALS SHORT SLEEVE GRAPHIC",
+      price: 849,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK TRAINING WOR MESH TIGHTS",
+      price: 1550,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING OSR HIGH-IMPACT BRA",
+      price: 1399,
+      strikedoffprice: 6599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK TRAINING ACTIVE CORE BACKPACK - MEDIUM",
+      price: 949,
+      strikedoffprice: 1899,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PRINT LEGGINGS ",
+      price: 1549,
+      strikedoffprice: 1799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK POLYTIGHT",
+      price: 1249,
+      strikedoffprice: 1749,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK SLEEVE TEE",
+      price: 699,
+      strikedoffprice: 1245,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC LEGGINGS",
+      price: 1399,
+      strikedoffprice: 2799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC TIGHTS",
+      price: 1099,
+      strikedoffprice: 1249,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK SHOW SHOES",
+      price: 1249,
+      strikedoffprice: 1489,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S CLASSIC REEBOK PANTS",
+      price: 1459,
+      strikedoffprice: 1235,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING RUN ESSENTIALS SHORT SLEEVE GRAPHIC",
+      price: 849,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK TRAINING WOR MESH TIGHTS",
+      price: 1550,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING OSR HIGH-IMPACT BRA",
+      price: 1399,
+      strikedoffprice: 6599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK TRAINING ACTIVE CORE BACKPACK - MEDIUM",
+      price: 949,
+      strikedoffprice: 1899,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PRINT LEGGINGS ",
+      price: 1549,
+      strikedoffprice: 1799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK POLYTIGHT",
+      price: 1249,
+      strikedoffprice: 1749,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK SLEEVE TEE",
+      price: 699,
+      strikedoffprice: 1245,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC LEGGINGS",
+      price: 1399,
+      strikedoffprice: 2799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC TIGHTS",
+      price: 1099,
+      strikedoffprice: 1249,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK SHOW SHOES",
+      price: 1249,
+      strikedoffprice: 1489,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S CLASSIC REEBOK PANTS",
+      price: 1459,
+      strikedoffprice: 1235,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING RUN ESSENTIALS SHORT SLEEVE GRAPHIC",
+      price: 849,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK TRAINING WOR MESH TIGHTS",
+      price: 1550,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING OSR HIGH-IMPACT BRA",
+      price: 1399,
+      strikedoffprice: 6599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK TRAINING ACTIVE CORE BACKPACK - MEDIUM",
+      price: 949,
+      strikedoffprice: 1899,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PRINT LEGGINGS ",
+      price: 1549,
+      strikedoffprice: 1799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK POLYTIGHT",
+      price: 1249,
+      strikedoffprice: 1749,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK SLEEVE TEE",
+      price: 699,
+      strikedoffprice: 1245,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK PP TIGHTS",
+      price: 749,
+      strikedoffprice: 1299,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC LEGGINGS",
+      price: 1399,
+      strikedoffprice: 2799,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK CLASSIC TIGHTS",
+      price: 1099,
+      strikedoffprice: 1249,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK SHOW SHOES",
+      price: 1249,
+      strikedoffprice: 1489,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S CLASSIC REEBOK PANTS",
+      price: 1459,
+      strikedoffprice: 1235,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING RUN ESSENTIALS SHORT SLEEVE GRAPHIC",
+      price: 849,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK TRAINING WOR MESH TIGHTS",
+      price: 1550,
+      strikedoffprice: 2599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "WOMEN'S REEBOK RUNNING OSR HIGH-IMPACT BRA",
+      price: 1399,
+      strikedoffprice: 6599,
+    },
+    {
+      image_url:
+        "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+      name: "UNISEX REEBOK TRAINING ACTIVE CORE BACKPACK - MEDIUM",
+      price: 949,
+      strikedoffprice: 1899,
+    },
+  ];
+setData();
+function setData(){
+  womensData.map(function(elem){
+    let div = document.createElement("div");
+
+    let image = document.createElement("img");
+    image.setAttribute('src',elem.image_url)
+    image.setAttribute('alt',elem.id)
+    let name = document.createElement("h5");
+    name.innerText = elem.name;
+    let price = document.createElement("p");
+    price.innerText = elem.price;
+    let strikedoffprice = document.createElement("p");
+    strikedoffprice.innerText = elem.strikedoffprice;
+    let priceBox = document.createElement("div");
+    priceBox.append(price,strikedoffprice);
+    div.append(image,name,priceBox)
+    document.getElementById("womens").append(div);
+  })
+}
