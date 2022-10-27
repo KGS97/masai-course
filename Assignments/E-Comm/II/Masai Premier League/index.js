b4f5476ac69e0e1897998fcaf5f3ed614f9e5ab9
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-10-27 22:50:30 +0530

    Completed setData function on homepage

--- Assignments/E-Comm/II/Masai Premier League/index.js
+++ Assignments/E-Comm/II/Masai Premier League/index.js
@@ -1,2 +1,24 @@
 // write js code here corresponding to index.html
 // You should add submit event on the form
+let arr = JSON.parse(localStorage.getItem("schedule")) || [];
+
+document.getElementById("masaiForm").addEventListener('submit',setData);
+
+function setData(){
+    event.preventDefault();
+    let obj = {};
+    let matchNum = document.getElementById("matchNum").value;
+    obj.matchNum = matchNum;
+    let teamA = document.getElementById("teamA").value;
+    obj.teamA = teamA;
+    let teamB = document.getElementById("teamB").value;
+    obj.teamB = teamB;
+    let date = document.getElementById("date").value;
+    obj.date = date;
+    let venue = document.getElementById("venue").value;
+    obj.venue = venue;
+    
+    arr.push(obj);
+    localStorage.setItem("arr", JSON.stringify(arr));
+    console.log(arr)
+}
