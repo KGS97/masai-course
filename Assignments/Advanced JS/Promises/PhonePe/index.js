b8dc10a95081022a8cfa0398b013606c2e167505
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-11-15 21:13:05 +0530

    Completed the phonepe webapp without the promises

---
+++ Assignments/Advanced JS/Promises/PhonePe/index.js
@@ -0,0 +1,100 @@
+let number = false;
+// var myPromise = new Promise((res, rej) => {
+//   if (number) {
+//     res();
+//   }
+// });
+defaultUI();
+
+function proceed() {
+  let num = document.getElementById("input").value;
+  loadingUI();
+  if (!number) setTimeout(NumberII, 2000, num);
+  else {
+    setTimeout(otp, 2000, num);
+  }
+}
+
+function loadingUI() {
+  document.getElementById("payment").innerHTML = "";
+  let img = document.createElement("img");
+  img.setAttribute(
+    "src",
+    "https://acegif.com/wp-content/uploads/loading-103.gif"
+  );
+  img.setAttribute("id", "loader");
+  document.getElementById("payment").append(img);
+}
+
+function defaultUI() {
+  document.getElementById("payment").innerHTML = "";
+  let input = document.createElement("input");
+  let button = document.createElement("button");
+  input.setAttribute("type", "text");
+  input.setAttribute("id", "input");
+  input.setAttribute("placeholder", "Please enter the receiver's phone number");
+  button.setAttribute("class", "btn");
+  button.setAttribute("id", "proceed");
+  button.addEventListener("click", proceed);
+  button.innerText = "Proceed";
+  document.getElementById("payment").append(input, button);
+  if (number) input.setAttribute("placeholder", "Please enter 4 digit otp");
+}
+function NumberII(num) {
+  if (num > 999999999) {
+    loadingUI();
+    number = true;
+    setTimeout(defaultUI, 2000);
+  } else {
+    loadingUI();
+    setTimeout(tryAgain, 2000);
+  }
+}
+let otpN = false;
+function otp(num) {
+  if (num == 1234) {
+    otpN = true;
+    failorSucces();
+  } else {
+    tryAgain();
+  }
+}
+
+function tryAgain() {
+  document.getElementById("payment").innerHTML = "";
+  let image = document.createElement("img");
+  image.setAttribute("src", "error.png");
+  let p = document.createElement("p");
+  if (number && !otpN) {
+    p.innerText = "Please enter the correct OTP";
+  } else if (!number && !otpN) {
+    p.innerText = "Please enter a valid number";
+  } else {
+    p.innerHTML =
+      "Oh, Sorry the payment has failed.<br/>Please try again later.";
+  }
+  p.style.color = "white";
+  document.getElementById("payment").append(image, p);
+  setTimeout(defaultUI, 4000);
+}
+function failorSucces() {
+  let x = Math.random() * 10;
+  console.log(x);
+  if (x > 0) {
+    tryAgain();
+  } else {
+    successUI();
+  }
+}
+
+function fail() {}
+
+function successUI() {
+  document.getElementById("payment").innerHTML = "";
+  let image = document.createElement("img");
+  image.setAttribute("src", "check.jpg");
+  let p = document.createElement("p");
+  p.innerText = "Congrats! Your payment was successful";
+  p.style.color = "white";
+  document.getElementById("payment").append(image, p);
+}
