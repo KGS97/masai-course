ccd91f7325f2436b7905c1357edcc39a85342ea9
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-11-25 00:26:12 +0530

    Completed the rOTD page

--- Assignments/Advanced JS/Import Export/Food App/utils/fetchData.js
+++ Assignments/Advanced JS/Import Export/Food App/utils/fetchData.js
@@ -10,12 +10,12 @@ export default function (q, url) {
 function getData(q, data) {
   if (q == "rOTD") {
     showrOTD(data.meals[0]);
+  } else {
+    data.meals.forEach((ele) => {
+      let { strMeal, strMealThumb } = ele;
+      showData(strMeal, strMealThumb);
+    });
   }
-
-  data.meals.forEach((ele) => {
-    let { strMeal, strMealThumb } = ele;
-    showData(strMeal, strMealThumb);
-  });
 }
 function showData(title, thumbnail) {
   document.getElementById("recipe").innerHTML = "";
@@ -28,4 +28,25 @@ function showData(title, thumbnail) {
   document.getElementById("recipe").append(container);
 }
 
-function showrOTD(data) {}
+function showrOTD(data) {
+  console.log(data);
+  let { strMeal, strMealThumb, strInstructions } = data;
+  showData(strMeal, strMealThumb);
+  for (let i = 1; i <= 20; i++) {
+    let ingredient = data[`strIngredient${i}`];
+    let quantity = data[`strMeasure${i}`];
+    if (ingredient == "" || quantity == " ") break;
+    else {
+      let tr0 = document.createElement("tr");
+      let tr1 = document.createElement("td");
+      tr1.innerText = ingredient;
+      let tr2 = document.createElement("td");
+      tr2.innerText = quantity;
+      tr0.append(tr1, tr2);
+      document.querySelector("tbody").append(tr0);
+    }
+  }
+  let instructions = document.createElement("p");
+  instructions.innerText = strInstructions;
+  document.getElementById("instructions").append(instructions);
+}
