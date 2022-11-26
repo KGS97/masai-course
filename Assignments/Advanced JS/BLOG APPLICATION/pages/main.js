32a768ac5c0ce34e7dc24b725c0526f9e219b8a6
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-11-27 01:24:48 +0530

    post request not working

--- Assignments/Advanced JS/BLOG APPLICATION/pages/main.js
+++ Assignments/Advanced JS/BLOG APPLICATION/pages/main.js
@@ -2,3 +2,6 @@ import getData from "../scripts/getDataUtil.js";
 getData();
 import navbar from "../componets/navbar.js";
 document.getElementById("nav").innerHTML = navbar();
+document.getElementById("create").addEventListener("click", () => {
+  window.location.href = "create.html";
+});
