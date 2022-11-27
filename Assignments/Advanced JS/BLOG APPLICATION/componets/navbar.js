0d49fda3d01a8f75e5f33684377c73270145ce03
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-11-27 12:48:51 +0530

    fixed abandoned req by using then

--- Assignments/Advanced JS/BLOG APPLICATION/componets/navbar.js
+++ Assignments/Advanced JS/BLOG APPLICATION/componets/navbar.js
@@ -1,6 +1,8 @@
 export default function () {
   return `<nav>
-      <img src="../icons/home.png" alt="Home" />
-      <img src="../icons/add.png" alt="Create" id="create" />
+<a href="main.html"> <img src="../icons/home.png" alt="Home" /></a>
+        <a href="create.html">
+          <img src="../icons/add.png" alt="Create" id="create"
+        /></a>
     </nav>`;
 }
--- Assignments/Advanced JS/BLOG APPLICATION/pages/create.js
+++ Assignments/Advanced JS/BLOG APPLICATION/pages/create.js
@@ -3,22 +3,22 @@ let id = localStorage.getItem("id");
 
 document.querySelector("button").addEventListener("click", postData);
 
-function postData() {
+async function postData() {
   let data = {
     title: document.getElementById("title-edit").value,
     body: document.getElementById("body-edit").value,
     author: document.getElementById("author-edit").value,
     gender: document.getElementById("gender-edit").value,
   };
-
   fetch("http://localhost:3000/blogs", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
+  }).then(() => {
+    window.location.href = "main.html";
   });
-  window.location.href = "main.html";
 }
 import navbar from "../componets/navbar.js";
 document.getElementById("nav").innerHTML = navbar();
