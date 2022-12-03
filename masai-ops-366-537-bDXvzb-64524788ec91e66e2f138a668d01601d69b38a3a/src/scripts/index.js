240cb2dfb2a8a3a9b92d896eb6b9c4ad229f1d08
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-03 12:44:06 +0530

    Done home page

--- masai-ops-366-537-bDXvzb-64524788ec91e66e2f138a668d01601d69b38a3a/src/scripts/index.js
+++ masai-ops-366-537-bDXvzb-64524788ec91e66e2f138a668d01601d69b38a3a/src/scripts/index.js
@@ -1,1 +1,86 @@
+import navbar from "./navbar.js";
+var page = 1;
+var totalPages = 0;
+window.onload = () => {
+  document.getElementById("navbar").innerHTML = navbar();
+  cartCount();
+  showData(1);
+  document.getElementById("next").addEventListener("click", () => {
+    if (page < totalPages) showData(++page);
+  });
+  document.getElementById("previous").addEventListener("click", () => {
+    if (page > 1) showData(--page);
+  });
+};
+function cartCount() {
+  document.getElementById("cart-count").innerText =
+    localStorage.getItem("cart-count") || 0;
+}
+async function showData(page, loaded) {
+  let data = await fetch(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products/?limit=6&page=${page}`
+  );
 
+  data = await data.json();
+  totalPages = data.totalPages;
+  data = loaded || data.data;
+  document.getElementById("sort-lth").addEventListener("click", () => {
+    sortlth(data);
+  });
+  document.getElementById("sort-htl").addEventListener("click", () => {
+    sorthtl(data);
+  });
+  var parent = document.getElementById("main_items");
+  parent.innerHTML = "";
+  data.forEach((ele) => {
+    let container = document.createElement("div");
+    container.className = "item";
+    let img = document.createElement("img");
+    img.src = ele.image;
+    let category = document.createElement("p");
+    category.innerText = "For " + ele.category;
+    let name = document.createElement("h4");
+    name.innerText = ele.title;
+    let brand = document.createElement("p");
+    brand.innerText = "Brand :- " + ele.brand;
+    let price = document.createElement("p");
+    brand.innerText = ele.price;
+    price.className = "price";
+    let button = document.createElement("button");
+    button.className = "add_to_cart";
+    button.addEventListener("click", add2C);
+    button.innerText = "Add to Cart";
+    container.append(img, category, name, brand, price, button);
+    parent.append(container);
+  });
+}
+
+function add2C() {
+  console.log("ok");
+}
+function sorthtl(data) {
+  console.log("htl");
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[i].price < data[j].price) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+  showData(1, data);
+}
+function sortlth(data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[i].price > data[j].price) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+  //console.log(data);
+  showData(1, data);
+}
--- masai-ops-366-537-bDXvzb-64524788ec91e66e2f138a668d01601d69b38a3a/src/scripts/navbar.js
+++ masai-ops-366-537-bDXvzb-64524788ec91e66e2f138a668d01601d69b38a3a/src/scripts/navbar.js
@@ -1,1 +1,19 @@
 // Export navbar from here
+export default function () {
+  return ` <nav
+        style="
+          color: white;
+          background-color: black;
+          display: flex;
+          justify-content: space-between;
+          padding:20px 25px;
+        "
+      >
+        <a href="index.html" id="home" style="color: white">Home</a>
+        <div id="cart" style="display: flex;
+          justify-content: space-between;width:5%">
+          <a href="cart.html" id="cart" style="color: white">Cart</a>
+          <div id="cart-count"></div>
+        </div>
+      </nav>`;
+}
