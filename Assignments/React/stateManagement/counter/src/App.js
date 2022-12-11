8dd6c3810b1de21a623ddb165cea6682626378e7
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-11 15:25:02 +0530

    starting ToDo component

--- Assignments/React/stateManagement/counter/src/App.js
+++ Assignments/React/stateManagement/counter/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <Counter />
-      <InputBox />
+      {/* <Counter />
+      <InputBox /> */}
     </div>
   );
 }
