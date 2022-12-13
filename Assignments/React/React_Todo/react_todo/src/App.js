118b4fdea6b2735fc754847975414b9be4798952
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-14 01:16:16 +0530

    Completed the input card

--- Assignments/React/React_Todo/react_todo/src/App.js
+++ Assignments/React/React_Todo/react_todo/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Card />
     </div>
   );
 }
