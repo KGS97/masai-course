608facde638afb4cbb8a8a1eee5817096b52b609
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-13 23:23:14 +0530

    Completed the navbar

---
+++ Assignments/React/React_Todo/react_todo/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  let style = {
+    backgroundColor: "#5b04ba",
+    marginTop: "0",
+    padding: "20px",
+    color: "white",
+  };
+  return (
+    <div style={style}>
+      <p style={{ margin: "0 auto", fontWeight: "100", fontSize: "30px" }}>
+        Todo App
+      </p>
+    </div>
+  );
+}
+export default Navbar;
