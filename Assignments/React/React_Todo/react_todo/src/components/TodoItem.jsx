489666d786d19a91177e9cdb0ea8fb04d265eb3a
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-14 14:26:47 +0530

    Completed until delete and update

--- Assignments/React/React_Todo/react_todo/src/components/TodoItem.jsx
+++ Assignments/React/React_Todo/react_todo/src/components/TodoItem.jsx
@@ -1,18 +1,42 @@
 function TodoItem(props) {
   let { item, index } = props;
-  //console.log(index, item);
   return (
-    <table>
-      <tr>
-        <td>
-          <input type="checkbox" className="checkboxes" onChange={event} />
-        </td>
-        <td>
-          <p>{item}</p>
-        </td>
-        <td></td>
-      </tr>
-    </table>
+    <tr style={tditem()}>
+      <td>
+        <input
+          type="checkbox"
+          className="checkboxes"
+          onChange={(event) => event.target.value}
+        />
+      </td>
+      <td>{item}</td>
+      <td style={buttonsStyle()}>
+        <i
+          style={{ cursor: "pointer" }}
+          className="fa-regular fa-pen-to-square"
+        ></i>
+        <i
+          style={{ cursor: "pointer" }}
+          className="fa-regular fa-trash-can"
+        ></i>
+      </td>
+    </tr>
   );
 }
 export default TodoItem;
+let tditem = () => {
+  let style = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  };
+  return style;
+};
+function buttonsStyle() {
+  let style = {
+    width: "7.5%",
+    display: "flex",
+    justifyContent: "space-around",
+  };
+  return style;
+}
--- Assignments/React/React_Todo/react_todo/src/components/TodoList.jsx
+++ Assignments/React/React_Todo/react_todo/src/components/TodoList.jsx
@@ -4,21 +4,38 @@ function TodoList(props) {
   let { list } = props;
   let style = {
     minHeight: "100px",
-    width: "50%",
+    width: "47.5%",
     boxShadow:
       "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
     margin: "25px auto",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    justifyContent: "space-evenly",
+    padding: "20px",
   };
   return (
     <div style={style}>
       {list.map((elem, index) => {
-        return <TodoItem item={elem} index={index} />;
+        return (
+          <table style={tableStyle()}>
+            <tbody>
+              <TodoItem item={elem} index={index} />
+            </tbody>
+          </table>
+        );
       })}
     </div>
   );
   //console.log(list);
 }
 export default TodoList;
+let tableStyle = () => {
+  let Style = {
+    padding: "5px",
+    width: "90%",
+    display: "flex",
+    flexDirection: "column",
+  };
+  return Style;
+};
