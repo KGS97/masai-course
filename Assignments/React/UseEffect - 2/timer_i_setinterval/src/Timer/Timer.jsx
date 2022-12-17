0c883508c724b8e5a31d05d5552fdf182ee96b6f
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-18 01:21:31 +0530

    stuck at how to unmount the component

--- Assignments/React/UseEffect - 2/timer_i_setinterval/src/Timer/Timer.jsx
+++ Assignments/React/UseEffect - 2/timer_i_setinterval/src/Timer/Timer.jsx
@@ -2,11 +2,15 @@ import { useState, useRef, useEffect } from "react";
 let Timer = () => {
   let [secs, SetSecs] = useState(0);
   let interval = useRef(null);
+  let pRef = useRef(null);
   useEffect(() => {
     if (interval.current) clearInterval(interval.current);
     interval.current = setInterval(() => {
       SetSecs((secs) => secs + 1);
     }, 1000);
+    return () => {
+      clearInterval(interval.current);
+    };
   }, [secs]);
 
   let stopTimer = () => {
@@ -14,9 +18,10 @@ let Timer = () => {
   };
   return (
     <>
-      <p>{secs}</p>
+      <p ref={pRef}> {secs}</p>
       <button onClick={() => SetSecs(secs + 1)}>Start</button>
       <button onClick={() => stopTimer()}>Stop</button>
+      <button>Show/Hide</button>
     </>
   );
 };
