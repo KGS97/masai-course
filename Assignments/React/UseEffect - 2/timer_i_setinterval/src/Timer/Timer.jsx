1b63e3cd837825540a43f0bd485a19bdd7e7bf83
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-18 01:11:16 +0530

    completed with secs as dependency

--- Assignments/React/UseEffect - 2/timer_i_setinterval/src/Timer/Timer.jsx
+++ Assignments/React/UseEffect - 2/timer_i_setinterval/src/Timer/Timer.jsx
@@ -3,25 +3,19 @@ let Timer = () => {
   let [secs, SetSecs] = useState(0);
   let interval = useRef(null);
   useEffect(() => {
+    if (interval.current) clearInterval(interval.current);
     interval.current = setInterval(() => {
       SetSecs((secs) => secs + 1);
     }, 1000);
-  }, []);
-  let startTimer = () => {
-    if (interval.current) clearInterval(interval.current);
-    interval.current = setInterval(() => {
-      SetSecs((secs) => {
-        return secs + 1;
-      });
-    }, 1000);
-  };
+  }, [secs]);
+
   let stopTimer = () => {
     clearInterval(interval.current);
   };
   return (
     <>
       <p>{secs}</p>
-      <button onClick={() => startTimer()}>Start</button>
+      <button onClick={() => SetSecs(secs + 1)}>Start</button>
       <button onClick={() => stopTimer()}>Stop</button>
     </>
   );
