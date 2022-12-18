1583ace9ae4aadb8ed7293ee2a0afed053c3ce63
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-19 01:51:15 +0530

    Timer component done

--- Assignments/React/useRef_hook_and_Forms/timer_stopwatch/src/components/Timer.js
+++ Assignments/React/useRef_hook_and_Forms/timer_stopwatch/src/components/Timer.js
@@ -1,8 +1,45 @@
 import { useRef, useState } from "react";
 let Timer = () => {
-  let [data, updateData] = useState({});
-  let checkboxRef = useRef(false);
-  return <></>;
+  let [secs, updateSecs] = useState(0);
+  let intervalRef = useRef(null);
+  let inputRef = useRef(null);
+  let StartTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      updateSecs((secs) => secs - 1);
+    }, 1000);
+  };
+  let StopTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+  return (
+    <>
+      <input ref={inputRef} type="text" />
+      <button
+        onClick={() => {
+          updateSecs(inputRef.current.value);
+          StartTimer();
+        }}
+      >
+        start
+      </button>
+      <button
+        onClick={() => {
+          StopTimer();
+        }}
+      >
+        stop
+      </button>
+      <button
+        onClick={() => {
+          updateSecs(0);
+        }}
+      >
+        Reset
+      </button>
+      <p>{secs}</p>
+    </>
+  );
 };
 
 export default Timer;
