043a8d36ff63d18af7bb9c2e993715c6804d0266
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-18 22:31:45 +0530

    starting the react app

---
+++ Assignments/React/useRef_hook_and_Forms/forms - Copy (2) - Copy/src/components/Form.js
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+let Form = () => {
+  let [data, updateData] = useState({});
+  let checkboxRef = useRef(false);
+  let onChangeHandler = (props) => {
+    let prop = { ...data, ...props };
+    console.log(prop);
+    updateData(prop);
+  };
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter Name"
+        onChange={(e) => {
+          let data = { name: e.target.value };
+          onChangeHandler(data);
+        }}
+      />
+      <select
+        value="Select Gender"
+        onChange={(e) => {
+          let data = { gender: e.target.value };
+          onChangeHandler(data);
+        }}
+      >
+        <option value="">Select Gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+      <div>
+        {" "}
+        <p>Are you married?</p>{" "}
+        <input
+          type="checkbox"
+          onClick={(e) => {
+            checkboxRef.current = !checkboxRef.current;
+            console.log(checkboxRef.current);
+            let data = {
+              maritalStaus: checkboxRef.current,
+            };
+            onChangeHandler(data);
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Form;
