3a1694a14a2b937bb1b74483ad7c94053bd3b166
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-18 16:51:37 +0530

    completed input focus

---
+++ Assignments/React/useRef_hook_and_Forms/forms - Copy (2)/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "./components/Form";
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
