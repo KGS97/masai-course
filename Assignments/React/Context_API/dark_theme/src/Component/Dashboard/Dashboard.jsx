dd574eaec9099d4313e079bd6743ddc137f08d29
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-22 23:33:52 +0530

    completed DashboardHead

--- Assignments/React/Context_API/dark_theme/src/Component/Dashboard/Dashboard.jsx
+++ Assignments/React/Context_API/dark_theme/src/Component/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DashboardHead from "./DashboardHead/DashboardHead";
 import CardsContext from "../../Context/CardsContext";
 import ThemeContext from "../../Context/ThemeContext";
+import DashboardBody from "../Dashboard/Dashboard_Body/Dashboard_Container/";
 function Dashboard(props) {
   let Cards = [
     {
@@ -21,12 +22,18 @@ function Dashboard(props) {
     },
   ];
   let [theme, ChangeTheme] = React.useState("Light");
+  colorTheBody();
+  function colorTheBody() {
+    document.querySelector("body").style.backgroundColor =
+      theme === "Light" ? "#F5F6FA" : "#1F2327";
+  }
 
   return (
     <div className={theme === "Light" ? "Light" : "Dark"}>
       <CardsContext.Provider value={Cards}>
         <ThemeContext.Provider value={{ theme, ChangeTheme }}>
           <DashboardHead />
+          <DashboardBody />
         </ThemeContext.Provider>
       </CardsContext.Provider>
     </div>
